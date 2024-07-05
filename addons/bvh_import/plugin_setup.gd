d755aafa8505116835b6e104720ea54ae8141a2d
Pierce Brooks <piercebrooks@rocketmail.com>
2024-07-05 13:51:54 -0700

    initial commit

---
+++ addons/bvh_import/plugin_setup.gd
@@ -0,0 +1,26 @@
+@tool
+extends EditorPlugin
+
+#
+# Plugin boilerplate and housekeeping.
+#
+
+var dock
+
+func _enter_tree():
+	# Initialization of the plugin goes here
+	# Load the dock scene and instance it
+	dock = preload("res://addons/bvh_import/dock.tscn").instantiate()
+	dock.editor_interface = get_editor_interface()
+	
+	# Add the loaded scene to the docks
+	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
+	
+func _exit_tree():
+	# Free memory.  Clean up signals.
+	if dock == null:
+		print("Dock is null.")
+	#dock.queue_free()
+	remove_control_from_docks(dock)
+
+
